7e13bc23c9e107232d3ce50b94d52684c78117bf
Jeff Langr <jeff.langr@outpace.com>
2016-11-09 11:05:36 -0700

    updated instructions for NameNormalizerTest

--- library/src/test/java/util/NameNormalizerTest.java
+++ library/src/test/java/util/NameNormalizerTest.java
@@ -6,7 +6,7 @@ import org.junit.*;
 
 // 1. Un-ignore the next commented-out test method.
 // 2. Run JUnit against *all* tests in the project.
-// 3. Did the current test fail? If not: You built too much code in a prior step. Discard code to that point and try again.
+// 3. Did the current test fail? If not: You built too much code in a prior step. Undo work for prior tests and try again.
 // 4. Make sure you are clear on why the test failed.
 // 5. Write only enough code to make that failing test pass (and not break any other tests).
 //    Did you write too much? Is there a simple way to get that test to pass???
